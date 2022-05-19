9f8c8584ff6f5c3ae391b6d89a7355dd4756d724
vanish125 <vanish125@gmail.com>
2022-05-19 15:13:51 +0300

    get screen from protobuf

--- screen.py
+++ screen.py
@@ -9,79 +9,87 @@ import numpy
 
 from PIL import Image
 
+import importlib
+
+protoflipper_module = importlib.import_module(
+    "flipperzero-protobuf-py.flipper_protobuf")
+
 image_path = "out"
 
 res_x = 128
 res_y = 64
 name = sys.argv[2]
 
-ser = serial.Serial(sys.argv[1])
-ser.baudrate = 230400
-ser.write(b"\nscreen_stream\r\n")   #once
-ser.read_until(bytes.fromhex('F0E1D2C3'))
-data = ser.read(int(res_x*res_y/8))
 
-get_bin = lambda x: format(x, 'b')
+def main():
+    # open serial port
+    flipper = serial.Serial(sys.argv[1], timeout=1)
+    flipper.baudrate = 230400
+    flipper.flushOutput()
+    flipper.flushInput()
+    flipper.timeout = None
+    flipper.read_until(b'>: ')
+    flipper.write(b"start_rpc_session\r")
+    flipper.read_until(b'\n')
 
-scr = numpy.zeros((res_x+1, res_y+1))
 
-x = y = 0
-basey = 0
+    proto = protoflipper_module.ProtoFlipper(flipper)
 
-for i in range(0, int(res_x*res_y/8)):
-    tmp = get_bin(data[i])
-    tmp = '0'*(8-len(tmp)) + tmp
-    tmp = tmp[::-1]     #reverse
-    
-    y = basey
-    x += 1
-    for c in tmp:
-        scr[x][y] = c
-        y += 1
-    
-    if (i + 1) % res_x == 0:
-        basey += 8
-        x = 0
 
-# Unicode
-# for y in range(0, res_y, 2):
-#     for x in range(1, res_x+1):
-#         if int(scr[x][y]) == 1 and int(scr[x][y+1]) == 1:
-#             print(u'\u2588', end='')
-#         if int(scr[x][y]) == 0 and int(scr[x][y+1]) == 1:
-#             print(u'\u2584', end='')
-#         if int(scr[x][y]) == 1 and int(scr[x][y+1]) == 0:
-#             print(u'\u2580', end='')
-#         if int(scr[x][y]) == 0 and int(scr[x][y+1]) == 0:
-#             print(' ', end='')
-#     print()
+    data = proto.cmd_gui_snapshot_screen()
 
-#export to image file:
+    get_bin = lambda x: format(x, 'b')
 
-scale = 3
-col_black = [0x11, 0x11, 0x11]
-col_white = [0xff, 0x8c, 0x29]
-col_bg = col_black
+    scr = numpy.zeros((res_x+1, res_y+1))
 
-img = numpy.zeros((res_y*scale, res_x*scale, 3), dtype=numpy.uint8)
-for y in range(0, res_y*scale):
-    for x in range(0, res_x*scale):
-        img[y][x] = col_bg
+    x = y = 0
+    basey = 0
 
-for y in range(0, res_y):
-    for x in range(1, res_x+1):
-        if int(scr[x][y]) == 1:
-            for yy in range(y*scale, (y+1)*scale):
-                for xx in range((x-1)*scale, x*scale):
-                    img[yy][xx] = col_black
-        else:
-            for yy in range(y*scale, (y+1)*scale):
-                for xx in range((x-1)*scale, x*scale):
-                    img[yy][xx] = col_white
-                    #img[y*scale][(x-1)*scale] = col_white
+    for i in range(0, int(res_x*res_y/8)):
+        tmp = get_bin(data[i])
+        tmp = '0'*(8-len(tmp)) + tmp
+        tmp = tmp[::-1]     #reverse
+        
+        y = basey
+        x += 1
+        for c in tmp:
+            scr[x][y] = c
+            y += 1
+        
+        if (i + 1) % res_x == 0:
+            basey += 8
+            x = 0
 
-im = Image.new('RGB', (res_x, res_y))
-im = Image.fromarray(img)
-im.save(f'{image_path}/{name}')
+    #export to image file:
 
-print('Saved to', name)
+    scale = 3
+    col_black = [0x11, 0x11, 0x11]
+    col_white = [0xff, 0x8c, 0x29]
+    col_bg = col_black
+
+    img = numpy.zeros((res_y*scale, res_x*scale, 3), dtype=numpy.uint8)
+    for y in range(0, res_y*scale):
+        for x in range(0, res_x*scale):
+            img[y][x] = col_bg
+
+    for y in range(0, res_y):
+        for x in range(1, res_x+1):
+            if int(scr[x][y]) == 1:
+                for yy in range(y*scale, (y+1)*scale):
+                    for xx in range((x-1)*scale, x*scale):
+                        img[yy][xx] = col_black
+            else:
+                for yy in range(y*scale, (y+1)*scale):
+                    for xx in range((x-1)*scale, x*scale):
+                        img[yy][xx] = col_white
+                        #img[y*scale][(x-1)*scale] = col_white
+
+    im = Image.new('RGB', (res_x, res_y))
+    im = Image.fromarray(img)
+    im.save(f'{image_path}/{name}')
+
+    print('Saved to', name)
+
+
+if __name__ == '__main__':
+    main()
