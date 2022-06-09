13c2fa269d5ee9f8c8e74c0d6a8eb7ab0636a2b7
JsRend <pavel.nahabin.g@gmail.com>
2022-06-09 14:54:25 +0300

    Development of GUI test

--- const.py
+++ const.py
@@ -1,7 +1,20 @@
 class cnt:
-    MUTE        = 1
-    VOL_UP      = 2
-    VOL_DOWN    = 3
-    CH_UP       = 4
-    CH_DOWN     = 5
+    MUTE        = 0
+    VOL_UP      = 1
+    VOL_DOWN    = 2
+    CH_UP       = 3
+    CH_DOWN     = 4
+class f_dict:
+    fr_dict = {
+            '433.92': 0, '433.92': 1, '433.92': 2, '433.92': 3,
+            '433.92': 4, '300': 5,  '433.92': 6,  '433.92': 7, 
+            '315': 8,  '433.92': 9,  '315': 10,  '390': 11, 
+            '310': 12, '315': 13, '390': 14
+            }
+    signal_dict = {
+            'Princeton_433': 0, 'Nice_FLo_12bit_433': 1, 'Nice_FLo_24bit_433': 2, 'CAME_12bit_433': 3,
+            'CAME_24bit_433': 4,  'Linear_300': 5,  'CAME_TWEE': 6, 'Gate_TX_433': 7, 'DoorHan_315': 8,
+            'DoorHan_433': 9, 'LiftMaster_315': 10, 'LiftMaster_390': 11, 'Security+2.0_310': 12, 'Security+2.0_315': 13,  
+            'Security+2.0_390':14
+        }
     
